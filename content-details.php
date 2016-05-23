ca4b3524192be0970ef98c5f65027202fd343b17
Curtis Peters <curtis005@gmail.com>
2016-05-24 00:06:29 +0700

    add contact me button to bottom of portfolio details

--- content-details.php
+++ content-details.php
@@ -2,8 +2,12 @@
 <div class="portfolio-item-details the-details small-12 columns holder flush " data-window=<?php echo $post->ID ?>>
     <div class="small-6 columns even-height portfolio-details-column">
         <?php the_content( ); ?>
+        <div class="cta ">
+            <a href="mailto:curtis005@gmail.com" class="more-content centered"><span>Contact Me</span></a>
+          </div>
     </div>
     <div class="small-6 columns even-height image-space" style="background: url(<?php echo $full_image[0]; ?>)">
          <p>&nbsp;</p>
+
     </div>
 </div>
