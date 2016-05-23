d2c3c83bbd3420f00444100a48d962ef716049aa
Curtis Peters <curtis005@gmail.com>
2016-05-23 18:58:43 +0700

    paint body background theme blue

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,35 @@ $(document).foundation();
 
 
 jQuery(document).ready(function ($){
+
         
     /**
     *
     * automatically size height of header to height of window
     *
     **/
+    function match_window_height(){
+        var wheight = $(window).height();
+        var wwidth = $(window).width();
+        var cardHeight = $('.header-cards').height();
+        var dtop = (wheight/2) - (cardHeight);
+        if(wwidth >= 640 ) {// if it is a large screen
+            $('.header').height(wheight); //make header full-screen
+            $('.header').css('padding-top',dtop);
 
-    var wheight = $(window).height();
-    var wwidth = $(window).width();
-    var cardHeight = $('.header-cards').height();
-    var dtop = (wheight/2) - (cardHeight);
-    if(wwidth >= 640 ) {// if it is a large screen
-        $('.header').height(wheight); //make header full-screen
-        $('.header').css('padding-top',dtop);
-
-        // align footer to bottom of screen
-        $('.footer').addClass('align-screen-bottom');
+            // align footer to bottom of screen
+            $('.footer').addClass('align-screen-bottom');
+        }
     }
+    /**
+     *
+     * Resize height of content area to match screen on load
+     * and when the window is resized
+     *
+     */
+    
+    match_window_height();
+    $(window).resize(match_window_height);
 
     /**
     *
