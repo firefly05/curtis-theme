a227f58b5e197460dea20227aac774f6deb21471
Curtis Peters <curtis005@gmail.com>
2016-05-23 19:33:36 +0700

    grab project summary from the_content

--- content-details.php
+++ content-details.php
@@ -1,18 +1,7 @@
 
 <div class="portfolio-item-details the-details small-12 columns holder flush " data-window=<?php echo $post->ID ?>>
     <div class="small-6 columns even-height portfolio-details-column">
-        <ul>
-            <li class="li-title">Features:</li>
-            <li>Customize account with profile picture</li>
-        </ul>
-        <ul>
-            <li class="li-title">Technical Stuff:</li>
-            <li>Form validation with nonce</li>
-            <li>Live content validation and update with JQuery/Ajax</li>
-            <li>Customized automated email messages</li>
-            <li>Custom user roles and privileges</li>
-            <li>Generates random opener from database for each new story</li>
-        </ul>
+        <?php the_content( ); ?>
     </div>
     <div class="small-6 columns even-height image-space" style="background: url(<?php echo $full_image[0]; ?>)">
          <p>&nbsp;</p>
