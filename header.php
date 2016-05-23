fb50e04de95236832b1a20d82a4362113371e575
Curtis Peters <curtis005@gmail.com>
2016-05-23 21:43:10 +0700

    link mailto function to contact me button

--- header.php
+++ header.php
@@ -85,7 +85,7 @@
             
           </div><!-- header cards -->
           <div class="cta small-pull-2">
-            <a href="#content" class="more-content centered">Contact Me</a>
+            <a href="mailto:curtis005@gmail.com" class="more-content centered">Contact Me</a>
           </div>
           
         </div><!-- base-card header -->
