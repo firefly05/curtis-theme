16cec5424f871b743289dd66c897cc141d7fb5a2
Curtis Peters <curtis005@gmail.com>
2016-05-24 15:42:58 +0700

    update site title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Curtis Peters | Toronto Full-stack web developer</title>
+    <title>Curtis Peters | Toronto WordPress developer</title>
 
     <?php wp_head(); ?>
    
